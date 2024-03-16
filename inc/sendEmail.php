4741e74ea83c347bb65f3d4a8c51b90bb566523a
Hassan Zaib Jadoon <Hassanzaibjadoon2004@gmail.com>
2024-03-16 18:10:02 +0500

    Update sendEmail.php

--- inc/sendEmail.php
+++ inc/sendEmail.php
@@ -1,7 +1,7 @@
 ﻿<?php
 
 // Replace this with your own email address
-$siteOwnersEmail = 'user@website.com';
+$siteOwnersEmail = 'hassanzaibjadoon2004@gmail.com';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
